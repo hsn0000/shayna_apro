24575457ed2419d0a75d75043ec65fe8b8271089
Husin <Muhamad.husin0412@gmail.com>
2021-04-19 12:03:58 +0700

    hallo everyone

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayana />
+    <PartnerShaya />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomanShayna from "@/components/WomanShayna.vue";
+import InstaShayana from "@/components/InstaShayana.vue";
+import PartnerShaya from "@/components/PartnerShaya";
+import FooterShayna from "@/components/FooterShayna";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayana,
+    PartnerShaya,
+    FooterShayna,
+  },
+};
+</script>
